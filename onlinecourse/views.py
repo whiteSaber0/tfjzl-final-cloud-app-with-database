bdde0bed3eee12f2d30faafcf15ef746527d5824
whiteSaber0 <whitesaber0@gmail.com>
2026-07-15 15:21:17 -0500

    Update views.py

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -70,7 +70,16 @@ def check_if_enrolled(user, course):
             is_enrolled = True
     return is_enrolled
 
-
+def submit(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    user = request.user
+    enrollment = Enrollment.objects.get(user=user, course=course)
+    submission = Submission.objects.create(enrollment=enrollment)
+    choices = extract_answers(request)
+    submission.choices.set(choices)
+    submission_id = submission.id
+    
+    return HttpResponseRedirect(reverse('onlinecourse:exam_result', args=(course_id, submission_id,)))
 # CourseListView
 class CourseListView(generic.ListView):
     template_name = 'onlinecourse/course_list_bootstrap.html'
@@ -132,6 +141,34 @@ def extract_answers(request):
         # For each selected choice, check if it is a correct answer or not
         # Calculate the total score
 #def show_exam_result(request, course_id, submission_id):
+
+def show_exam_result(request, course_id, submission_id):
+    context = {}
+    course = get_object_or_404(Course, pk=course_id)
+    submission = Submission.objects.get(id=submission_id)
+    choices = submission.choices.all()
+
+    total_score = 0
+    # Assuming course has related questions
+    questions = course.question_set.all()  
+
+    for question in questions:
+        # Get all correct choices for the question
+        correct_choices = question.choice_set.filter(is_correct=True)  
+        # Get the user's selected choices for the question
+        selected_choices = choices.filter(question=question)  
+
+        # Check if the selected choices are the same as the correct choices
+        if set(correct_choices) == set(selected_choices):
+            # Add the question's grade only if all correct answers are selected
+            total_score += question.grade  
+
+    context['course'] = course
+    context['grade'] = total_score
+    context['choices'] = choices
+
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
+
 
 
 
